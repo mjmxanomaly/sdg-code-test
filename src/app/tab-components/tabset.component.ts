2a78ba5e276806a732a59a333d5f81844f0b64ad
mjmxanomaly <mjmxanomaly@gmail.com>
2017-05-26 16:08:19 -0700

    Tabset functionality completed

--- src/app/tab-components/tabset.component.ts
+++ src/app/tab-components/tabset.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { Component, Input, ViewChildren, ContentChildren, QueryList, ElementRef, Renderer, AfterViewInit, AfterContentInit } from '@angular/core';
 import { TabComponent } from './tab.component';
 
 @Component({
   selector: 'tabset',
   template: `
     <div class="tab-set">
-      <ul>
+      <ul class="tab-header">
         <li
           *ngFor="let header of tabClass; let i = index"
-          [class.active]="header.active">
+          [class.active]="header.active"
+          #activeTrigger>
           <a
             (click)="tabClicked(header, i)"
             class="tab-btn">
@@ -23,30 +24,44 @@ import { TabComponent } from './tab.component';
   `,
   styles: []
 })
-export class TabsetComponent implements AfterContentInit {
+export class TabsetComponent implements AfterViewInit, AfterContentInit {
 
   @Input('tabHeader') tabClass:any;
-
+  @ViewChildren('activeTrigger') activeTrigger: QueryList<ElementRef>;
   @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
 
-  constructor() { }
+  constructor(private renderer: Renderer) { }
+
+  ngAfterViewInit() {
+    const activeClass = this.activeTrigger.toArray();
+    const lastItem = activeClass.length - 1;
+
+    if(activeClass.length > 0) {
+      this.renderer.setElementClass(activeClass[lastItem].nativeElement, "active", true);
+    }
+  }
 
   ngAfterContentInit() {
     const tabset = this.tabs.toArray();
     const actives = this.tabs.filter(t => { return t.active });
+    let lastItem = tabset.length - 1;
 
     setTimeout(() => {
       if(actives.length > 1) {
         console.error(`Multiple active tabs set 'active'`);
       } else if(!actives.length && tabset.length) {
-        tabset[0].active = true;
+        tabset[lastItem].active = true;
       }
     }, 300);
   }
 
   tabClicked(header, i) {
     const tabset = this.tabs.toArray();
+    const activeClass = this.activeTrigger.toArray();
     tabset.forEach(tab => tab.active = false);
+    activeClass.forEach(active => this.renderer.setElementClass(active.nativeElement, "active", false));
+    // header.active = true;
+    this.renderer.setElementClass(activeClass[i].nativeElement, "active", true);
     tabset[i].active = true;
   }
 
